5a5d78e65a8daf48378b5893b21c2b3965249526
Artem Krylysov <kadotex@gmail.com>
2017-10-28 21:52:12 -0400

    Write errors to stderr

--- cmd/hexunescape/main.go
+++ cmd/hexunescape/main.go
@@ -20,13 +20,13 @@ func main() {
 	if path := flag.Arg(0); path != "" {
 		var err error
 		if in, err = os.Open(path); err != nil {
-			fmt.Println(err)
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 		defer in.Close()
 	}
 	if _, err := io.Copy(os.Stdout, hexunescape.NewReader(in)); err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
